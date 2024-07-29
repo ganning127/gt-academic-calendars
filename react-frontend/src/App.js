59a16e9ce3d66e02049b0d1254c08bf16f759649
ganning127 <ganningxu@gmail.com>
2024-07-29 03:12:43 -0400

    fix ical parsing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -9,6 +9,19 @@ import Fall2024Ics from './assets/fall-2024.ics';
 import Spring2025Ics from './assets/spring-2025.ics';
 import { CalendarCard } from './components/CalendarCard';
 
+const calendarsToRender = [
+  {
+    title: "Fall 2024 Academic Calendar",
+    link: Fall2024Ics,
+    calId: "OWVmMmZiNTI3ODE1YzI1ZjNlNmE1NGRmODVmZTgxYjNmM2JhZjNmYWZlOThiZWYxNDdjNTJmODM3ZjNiMjNjN0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+  },
+  {
+    title: "Spring 2025 Academic Calendar",
+    link: Spring2025Ics,
+    calId: "N2VlNWI2ZWY2ODYzZGIwNzg4ZGUxNTFjNjY4NDRlN2UyZTg5MDNiMjNjYmE2MDcwMjg5ZTIyYjNkNDE0N2Q3Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+  }
+];
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
@@ -17,22 +30,20 @@ function App() {
         <Heading>
           Georgia Tech Academic Calendars
         </Heading>
-        <Text fontSize='md' mt={2} mb={4}>All data is pulled from the <Link color='yellow.600' href="https://registrar.gatech.edu/calendar/">registrar&apos;s academic calendar</Link>.
-          All source code is available <Link color='yellow.600' href="https://github.com/ganning127/gt-academic-calendars">on my GitHub</Link>.
+        <Text fontSize='md' mt={2} mb={4}>All data is pulled from the <Link color='yellow.600' href="https://registrar.gatech.edu/calendar/" target="_blank">registrar&apos;s academic calendar</Link>.
+          Source code is available <Link color='yellow.600' href="https://github.com/ganning127/gt-academic-calendars" target="_blank">on my GitHub</Link>.
         </Text>
 
         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8}>
-          <CalendarCard
-            title="Fall 2024 Academic Calendar"
-            link={Fall2024Ics}
-            embedSrc="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showTitle=1&showPrint=0&showCalendars=0&src=NzFlOTJhMGMwYTc4MjQ3ZGRhODc2ODE4MjNlNDNiNjNiNDkxYzE3OGQ4YjQ5OWQ0MGRiODRlOTg4YWM4ZjZmZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342"
-          />
-
-          <CalendarCard
-            title="Spring 2025 Academic Calendar"
-            link={Spring2025Ics}
-            embedSrc="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showTitle=1&showPrint=0&showCalendars=0&src=N2YwYjQ0ZTM3MDJmZDJmYTVmNGFjZjUxMzRmZjNkNmFmOWRiMjBjNjlhOWNlYjRjMGMwMDM5NTVkODAzZDAyNUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342"
-          />
+          {
+            calendarsToRender.map((calendar) => (
+              <CalendarCard
+                title={calendar.title}
+                link={calendar.link}
+                embedSrc={`https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&showTitle=1&showPrint=0&showCalendars=0&src=${calendar.calId}&color=%237CB342`}
+              />
+            ))
+          }
         </SimpleGrid>
 
       </Container>
