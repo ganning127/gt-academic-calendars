826e9716eb044d06573d4d05c6bbfb86d02e45c8
ganning127 <ganningxu@gmail.com>
2024-08-05 01:47:41 -0400

    change tab titles

--- react-frontend/src/components/Calendars/CalendarsRender.jsx
+++ react-frontend/src/components/Calendars/CalendarsRender.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { CalendarCard } from '../CalendarCard';
 import { CALENDARS_MAP } from '../../lib/constants';
@@ -6,11 +6,14 @@ import { CALENDARS_MAP } from '../../lib/constants';
 
 const CalendarsRender = () => {
   const { term } = useParams();
+  useEffect(() => {
+    document.title = `${term} | GT Academic Calendars`;
+
+  }, [term]);
 
   if (!CALENDARS_MAP[term]) {
     return <div>Calendar not found.</div>;
   }
-
   const calendar = CALENDARS_MAP[term];
   return (
     <>
